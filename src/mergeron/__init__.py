b9cd79c092c3eab548f38c6c41c3f5cbf70cb19b
smk <smk@localhost>
2026-08-13 00:22:24 -0400

    "chore(2026-08-13): update version"

--- src/mergeron/__init__.py
+++ src/mergeron/__init__.py
@@ -35,7 +35,7 @@ if TYPE_CHECKING:
 # Need the two-step re-export for sphinx/autoapi
 zipfile = zipfile_conditional
 
-VERSION = "2026.739840.0"
+VERSION = "2026.739840.1"
 
 __version__ = VERSION
 
